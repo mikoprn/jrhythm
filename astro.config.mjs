b3024c097bedb55e442ff329d53ae7c99b450e47
Mikoprn <b4gas24@gmail.com>
2025-05-25 13:51:16 +0700

    add file

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig, passthroughImageService } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    image: {
+    service: passthroughImageService(),
+    domains: ["astro.build"],
+  },
+});
